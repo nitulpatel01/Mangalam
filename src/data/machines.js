dec6bf1b7394294c789ec6a851ed7cf948cb9e7a
Nitulpatel15075 <nitulpatel15075@gmail.com>
2026-05-29 10:00:05 +0530

    Fix tool path casing and comment CNC tab in Infrastructure

--- src/data/machines.js
+++ src/data/machines.js
@@ -1,5 +1,5 @@
 const tools = Object.values(
-  import.meta.glob("../assets/Tools/*.{jpg,png,jpeg,webp}", {
+  import.meta.glob("../assets/tools/*.{jpg,png,jpeg,webp}", {
     eager: true,
   })
 ).map((img) => img.default);
@@ -73,5 +73,4 @@ export const machines = {
   //     img: getImage("machines_4", 3),
   //   },
   // ],
-
 };
